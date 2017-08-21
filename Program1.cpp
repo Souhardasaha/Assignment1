189a4332bb198be81940a31262ce26d6de4a91c0
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-21 14:51:49 +0530

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,15 @@
+/* C++ Program - Add Two Numbers */
+		
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	int a, b, sum;
+	cout<<"Enter two number :";
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"Sum of the two number is "<<sum;
+	return 0;
+}
